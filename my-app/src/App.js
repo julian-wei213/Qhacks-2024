aa30a82dc2d4cad54c4d12cb5c51458f253d7735
Francesca Adaobi <francesca7704@gmail.com>
2024-02-03 17:01:04 -0500

    Trial 1 for front page

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-
+/*
 function App() {
   return (
     <div className="App">
@@ -21,5 +21,20 @@ function App() {
     </div>
   );
 }
+*/
+function MyButton() {
+  return (
+    <button>I'm a button</button>
+  );
+}
 
-export default App;
+export default function MyApp() {
+  return (
+    <div>
+      <h1>Welcome to my app</h1>
+      <MyButton />
+    </div>
+  );
+}
+
+
