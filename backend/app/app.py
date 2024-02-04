2a00ca818f7c896b47677111d0e81bc271588dbb
DESKTOP-FOTQ5RK\shaya <rahman@myteabot.com>
2024-02-03 21:25:46 -0500

    Adding some routing stuff and functionality to the login button to test api requests

--- backend/app/app.py
+++ backend/app/app.py
@@ -1,8 +1,30 @@
-from flask import Flask
+from flask import Flask, request, render_template
+import numpy as np 
+import pickle
 
+# Create an app object of class Flask
 app = Flask(__name__)
 
-#@app.route('/')
+#Load the trained model from a pickle file
+model = pickle.load(open('models/model.pkl', 'rb'))
 
 
 
+@app.route('/')
+def home():
+    return render_template('.my-app\src\App.js')
+
+
+@app.route('predict', method=['POST'])
+def cnn_predict():
+    int_features = [float(x) for x in request.form.values()]
+    features = [np.array(int_features)]
+    prediction = model.predict(features)
+
+    output = prediction
+    
+    return render_template()
+
+
+if __name__ == '__main__':
+    app.run
