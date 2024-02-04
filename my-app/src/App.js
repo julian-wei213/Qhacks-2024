184bb21893b8af29583718057995dd2e4044dacc
julian-wei213 <julianwei@hotmail.ca>
2024-02-04 05:28:09 -0500

    Julian Frontend

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,138 +1,95 @@
-import * as React from 'react';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import AppBar from '@mui/material/AppBar';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import Grid from '@mui/material/Grid';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Link from '@mui/material/Link';
-import GlobalStyles from '@mui/material/GlobalStyles';
-import Container from '@mui/material/Container';
+import React, {Component} from "react" 
+import Axios from "axios" 
+import './Drip.css'
+import axios from "axios"
 
-function Copyright(props) {
-  return (
-    <Typography variant="body2" color="text.secondary" align="center" {...props}>
-      {'Copyright © '}
-      <Link color="inherit" href="https://mui.com/">
-        Your Website
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
+class App extends Component {
+
+  state = {
+    seletedFile: null
+  }
+
+  fileSelectedHandler = event => {
+    this.setState({
+      selectedFile: event.target.files[0]
+    })
+  }
+
+  fileUploadHandler = () => {
+    const formData = new FormData();
+ 
+    // Update the formData object
+    formData.append(
+        'file',
+        this.state.selectedFile
+    );
+
+    // Details of the uploaded file
+    console.log(this.state.selectedFile);
+
+    // Request made to the backend api
+    // Send formData object
+    axios.post(" http://127.0.0.1:5000/home", formData)
+    .then(response => {
+      console.log('File uploaded successfully: ', response.data);
+    })
+    .catch(error => {
+        console.error('Error uploading file:', error);
+    });
+  }
+
+
+     // File content to be displayed after
+    // file upload is complete
+    fileData = () => {
+      if (this.state.selectedFile) {
+          return (
+              <div>
+                  <h2>File Details:</h2>
+                  <p>
+                      File Name:{" "}
+                      {this.state.selectedFile.name}
+                  </p>
+
+                  <p>
+                      File Type:{" "}
+                      {this.state.selectedFile.type}
+                  </p>
+
+                  <p>
+                      Last Modified:{" "}
+                      {this.state.selectedFile.lastModifiedDate.toDateString()}
+                  </p>
+              </div>
+          );
+      } else {
+          return (
+              <div>
+                  <br />
+                  <h4>
+                      Choose before Pressing the Upload
+                      button
+                  </h4>
+              </div>
+          );
+      }
+  };
+
+  render() {
+      return (
+          <div className="file-upload-container">
+              <h1>Doc Assist</h1>
+              <div className="result-section">
+              <form className="file-upload-form">
+                <label htmlFor="fileInput">Choose a file:</label>
+                <input type="file" onChange={this.fileSelectedHandler} />
+                <button type="button" onClick={this.fileUploadHandler}>Upload</button>
+              </form>
+              </div >
+              {this.fileData()}
+          </div >
+      );
+  }
 }
 
-
-// Function to handle login button click
-function handleLogin() {
-  // Make API request to Flask backend
-  fetch('/api/login', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({ /* Add any necessary data for login */ })
-  })
-  .then(response => response.json())
-  .then(data => {
-    console.log(data); // Handle the response data
-  })
-  .catch(error => {
-    console.error('Error:', error);
-  });
-}
-
-
-
-// TODO remove, this demo shouldn't need to reset the theme.
-const defaultTheme = createTheme();
-
-export default function App() {
-  return (
-    <ThemeProvider theme={defaultTheme}>
-      <GlobalStyles styles={{ ul: { margin: 0, padding: 0, listStyle: 'none' } }} />
-      <CssBaseline />
-      <AppBar
-        position="static"
-        color="default"
-        elevation={0}
-        sx={{ borderBottom: (theme) => `1px solid ${theme.palette.divider}` }}
-      >
-        <Toolbar sx={{ flexWrap: 'wrap' }}>
-          <Typography variant="h6" color="inherit" noWrap sx={{ flexGrow: 1 }}>
-            Company name
-          </Typography>
-          <nav>
-            <Link
-              variant="button"
-              color="text.primary"
-              href="#"
-              sx={{ my: 1, mx: 1.5 }}
-            >
-              Features
-            </Link>
-            <Link
-              variant="button"
-              color="text.primary"
-              href="#"
-              sx={{ my: 1, mx: 1.5 }}
-            >
-              Enterprise
-            </Link>
-            <Link
-              variant="button"
-              color="text.primary"
-              href="#"
-              sx={{ my: 1, mx: 1.5 }}
-            >
-              Support
-            </Link>
-          </nav>
-          <Button href="#" variant="outlined" sx={{ my: 1, mx: 1.5 }}>
-            Login
-          </Button>
-        </Toolbar>
-      </AppBar>
-
-      {/* Hero unit */}
-      <Container disableGutters maxWidth="sm" component="main" sx={{ pt: 8, pb: 6 }}>
-        <Typography
-          component="h1"
-          variant="h2"
-          align="center"
-          color="text.primary"
-          gutterBottom
-        >
-          Add here
-        </Typography>
-        <Typography variant="h5" align="center" color="text.secondary" component="p">
-          Add Stuff Here
-        </Typography>
-      </Container>
-      {/* End hero unit */}
-      <Container maxWidth="md" component="main">
-        <Grid container spacing={5} alignItems="flex-end">
-          
-
-
-        </Grid>
-      </Container>
-
-
-      {/* Footer */}
-      <Container
-        maxWidth="md"
-        component="footer"
-        sx={{
-          borderTop: (theme) => `1px solid ${theme.palette.divider}`,
-          mt: 8,
-          py: [3, 6],
-        }}
-      >
-        <Copyright sx={{ mt: 5 }} />
-      </Container>
-      {/* End footer */}
-    </ThemeProvider>
-  );
-}
+export default App;
